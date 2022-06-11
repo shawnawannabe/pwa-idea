fe16ee8b872ca41846e58530e45377b108202c72
shawn <shawnang167@gmail.com>
2022-06-11 23:16:25 +0800

    server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m
+
+go 1.16
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- server.go
+++ server.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	// name := r.FormValue("name")
+	// address := r.FormValue("address")
+
+	// fmt.Fprintf(w, "Name = %s\n", name)
+	// fmt.Fprintf(w, "Address = %s\n", address)
+
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Failed to load .env")
+	}
+	fmt.Printf("Server started at port 8080\n")
+
+	fileServer := http.FileServer((http.Dir(os.Getenv("STATIC_DIR"))))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/", formHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
