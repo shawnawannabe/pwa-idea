4f649d5a495d28422de608a9b30e6990115c55b3
shawn <shawnang167@gmail.com>
2022-06-12 22:27:34 +0800

    post request

--- server.go
+++ server.go
@@ -10,16 +10,17 @@ import (
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
 	fmt.Fprintf(w, "POST request successful")
-	// name := r.FormValue("name")
-	// address := r.FormValue("address")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
 
-	// fmt.Fprintf(w, "Name = %s\n", name)
-	// fmt.Fprintf(w, "Address = %s\n", address)
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 
 }
 
@@ -32,6 +33,6 @@ func main() {
 
 	fileServer := http.FileServer((http.Dir(os.Getenv("STATIC_DIR"))))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/", formHandler)
+	http.HandleFunc("/form", formHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
